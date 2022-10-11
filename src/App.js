35763aa369d535dc0faf49448fa4344903dd549e
Abdul Haady <haady@Abduls-MacBook-Pro.local>
2022-10-11 10:39:49 +0530

    Added Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import RootNavigator from './navigators';
-import rootReducer from './redux/reducers';
-import thunk from 'redux-thunk';
+import React from "react";
+import { Provider } from "react-redux";
+import RootNavigator from "./navigators";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
 function App() {
-  const store = createStore(rootReducer, applyMiddleware(thunk));
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: [thunk],
+  });
   return (
     <Provider store={store}>
       <RootNavigator />
